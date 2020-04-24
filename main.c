76aab279435a3ecc294341fe25e148c28ff57fbe
Chance McCormick <43156530+cmccorm8@users.noreply.github.com>
2020-04-24 11:18:55 -0600

    update main.c

--- main.c
+++ main.c
@@ -7,10 +7,9 @@ int main() {
     //Call user input function, and pass these variables as arguments
     //they will be filled with data from the user. Need to make a header 
     //file with function prototypes, and then the actual C file.
-    struct stats params;
+    struct stats record;
 
-    interactUser(params.characterName, params.characterClass, params.preferredWeapon, params.stats, 
-    params.height, params.hitPoints, params.isQuestLeader);
+    interactUser(&record);
 
     //Once arguments have been filled with data in the above function,
     //pass them to an XOR encoding function. Potentially write the encoded 
