efce1f2b79278510b817193c3fc1902b6cc72cc5
Chance McCormick <43156530+cmccorm8@users.noreply.github.com>
2020-04-24 11:17:46 -0600

    implemented linked list

--- interactUser.c
+++ interactUser.c
@@ -5,42 +5,86 @@
 #include "prototypes.h"
 #define MAX_CHAR_LIMIT 35
 
-void interactUser(char cN[], char cC[], char pW[], int st[], double ht, float hp, bool iQL) {
+void interactUser(struct stats* record) {
+
+    
+    struct stats* current = NULL;
+    record->next = NULL;
+    current = &record;//head of the linked list
 
     char *descriptions[] = {"Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"};
     char leaderResponse[4];
-    struct stats *current = NULL;
-    current->next = calloc(1, sizeof (char*));
-
+    
+    //head = calloc(1,sizeof(struct stats));
+    //head
+    
     printf("What is your player name?: ");
     fgets(current->characterName, MAX_CHAR_LIMIT, stdin);
+    //current = record->characterName;
+    current->next = calloc(1,sizeof(struct stats));
+    current = current->next;
 
     printf("What is your player class? (Warrior, Bard, Mage etc.?): ");
-    fgets(cC, MAX_CHAR_LIMIT, stdin);
+    fgets(current->characterClass, MAX_CHAR_LIMIT, stdin);
+    //current = record->characterClass;
+    current->next = calloc(1,sizeof(struct stats));
+    current = current->next;
+
     printf("What is your preferred weapon?: ");
-    fgets(pW, MAX_CHAR_LIMIT, stdin);
+    fgets(current->preferredWeapon, MAX_CHAR_LIMIT, stdin);
+    // TODO add no as a possible answer.
     printf("Are you the quest leader? ");
     fgets(leaderResponse, 4, stdin);
     
     if (strcmp("Yes", leaderResponse) == 0) {
-        iQL = true;
+        current->isQuestLeader = true;
     }
     else if (strcmp("yes", leaderResponse) == 0) {
-        iQL = true; 
+        current->isQuestLeader = true; 
     }
 
+    current->next = calloc(1,sizeof(struct stats));
+    current = current->next;
+
     printf("What is your player height? (Enter as decimal value, e.g. '5.9'): ");
-    scanf("%f", &ht);
+    scanf("%lf", &current->height);
+    current->next = calloc(1,sizeof(struct stats));
+    current = current->next;
+    
+    
     printf("What is your base hitpoint count? (Enter as number): ");
-    scanf("%f", &hp);
+    scanf("%f", &current->hitPoints);
+    current->next = calloc(1,sizeof(struct stats));
+    current = current->next;
    
    for (int i = 0; i < 6; i++) {
         char *pos = descriptions[i];
         printf("What is your %s score? (0-10): ", pos);
-        scanf("%d", &st[i]);
+        scanf("%d", &current->stats[i]);
+    }
+    
+
+    
+    for(current = &record; current != NULL; current = current->next)
+    {   
+        if(current->characterName != NULL)
+            printf("%s\n", current);
+        else if (current->characterClass != NULL)
+            printf("%s\n", current);
+        else if(current->preferredWeapon != NULL)
+            printf("%s\n", current);
+        else if(current->isQuestLeader !=NULL)
+            printf("%s\n", current);
+        else if(current->height > 0)
+            printf("%s\n", current);
+        else if(current->hitPoints > 0)
+            printf("%s\n", current);
+        else
+            printf("%s\n", current);
     }
 
+
     struct stats params;
-    saveFile(cN, cC, pW, st, ht, hp, iQL);
+    //saveFile(cN, cC, pW, st, ht, hp, iQL);
     
 }
